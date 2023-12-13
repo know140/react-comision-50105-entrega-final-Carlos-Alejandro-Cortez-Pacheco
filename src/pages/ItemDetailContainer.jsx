487302d8a15d641a3be27d596675b3642e9860a7
Carlos Cortez <thehome@live.com.ar>
2023-12-13 17:11:27 -0300

    centrado

--- src/pages/ItemDetailContainer.jsx
+++ src/pages/ItemDetailContainer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-
 import { useParams } from "react-router-dom";
 import { useSingleProduct } from "../hooks/useProducts";
 import { ItemQuantitySelector } from "../components";
 
 export const ItemDetailContainer = () => {
   const { productId } = useParams();
-
   const { product, loading, error } = useSingleProduct(productId);
 
   return (
-    <Card key={product.id} style={{ width: "18rem", margin: 20 }}>
-      <Card.Img variant="top" src={product.thumbnail} />
-      <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
-        <Card.Text>{product.description}</Card.Text>
-      </Card.Body>
-      <ItemQuantitySelector />
-    </Card>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "flex-start", height: "100vh", marginTop: "50px", marginLeft: "50px" }}>
+      <Card key={product.id} style={{ width: "20rem", padding: "10px" }}>
+        <Card.Img variant="top" src={product.thumbnail} />
+        <Card.Body style={{ textAlign: "center" }}>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.description}</Card.Text>
+          <ItemQuantitySelector />
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
+
